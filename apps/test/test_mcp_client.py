128ea43e11f9ed6c3b493640248050659b269846
yxj52 <1028530975@qq.com>
2025-05-12 23:41:09 +0800

    更新测试

--- apps/test/test_mcp_client.py
+++ apps/test/test_mcp_client.py
@@ -1,10 +1,3 @@
 from fastmcp import Client
 
-client = Client("my_server.py")
-
-async def call_tool(name: str):
-    async with client:
-        result = await client.call_tool("greet", {"name": name})
-        print(result)
-
-asyncio.run(call_tool("Ford"))
+client = Client("http://localhost:8000/mcp")
